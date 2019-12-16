7b26af0a5a955f1cb513e5a0c72f2738151338e3
amHind <45667940+amHind@users.noreply.github.com>
2019-12-16 13:46:39 +0100

    Update UDP_server.py

--- UDP_server.py
+++ UDP_server.py
@@ -9,6 +9,8 @@ UDP_PORT_NO = 5001
 socket_syn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 mtu=1500
 tab_segments= []
+i=1
+j=0
 
 socket_data = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -22,8 +24,7 @@ while 1:
     message= message.decode()
     print ("message:", message)
     if message == "SYN":
-        message = "SYN-ACK7000".encode()
-        socket_syn.sendto(message, addrclt)
+        socket_syn.sendto(b"SYN-ACK7000", addrclt)
 
     (message, addrclt) = socket_syn.recvfrom(3)
     message= message.decode()
@@ -32,15 +33,40 @@ while 1:
         break
 socket_data.bind((UDP_IP_ADDRESS, 7000))
 fichierrecu= socket_data.recv(mtu).decode()
-print(fichierrecu)
-f = open("fichiertest.txt", 'w')
-#------------------------------------------------- PARTIE A REFAIRE-----------------------
-print(os.path.getsize(fichierrecu))
-print(size)
-for i in range (size):
+f = open("fichiertest.txt", 'r+')
+f_size  = os.fstat(f.fileno()).st_size
+print ("taille fichier: ",f_size)
+#-----------------------------------REMPLISSAGE DU TABLEAU DE SEGMENTS A ENVOYER-----------------------
+
+while f.tell() < f_size:
     data=f.read(1500)
-    numseq= 00000 + int(i)
-    tab_segments.append(str(numseq)+data)
-    print(tab_segments)
+    if len(str(i))==1:
+        numseq= "00000" + str(i)
+    if len(str(i))==2:
+        numseq= "0000" + str(i)
+    if len(str(i))==3:
+        numseq= "000" + str(i)
+    if len(str(i))==4:
+        numseq= "00" + str(i)
+    if len(str(i))==5:
+        numseq= "0" + str(i)
+    if len(str(i))==6:
+        numseq= str(i)
+    tab_segments.append(numseq+data)
+    i+=1
+print ("longueur du tab: ", len(tab_segments))
+#----------------------------------------ENVOI DU TABLEAU AU CLIENT ---------------------------------
+while j<len(tab_segments):
+    socket_data.sendto(tab_segments[j].encode(), addrclt)
+    ack_received= socket_data.recv(9).decode()
+    ack_received= ack_received[3:9] # on reçoit ACK0000N il faut extraire les 6 derniers elements
+    if ack_received == tab_segments[j][0:6]: # si le client a bien acquitté le dernier segment reçu, incrementer j pour envoyer le prochain segment
+        j+=1
+    else:
+        #SET UN TIMER PUIS RENVOYER
+#----------------------FIN DE TRANSFERT FICHIER---------------------------
+socket_data.sendto(b"FIN", addrclt)
+
+
 
 f.close()
