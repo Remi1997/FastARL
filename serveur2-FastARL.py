b40f14f750b8bd757bf3006acd5701a4a2fe5a20
amHind <45667940+amHind@users.noreply.github.com>
2020-01-29 13:09:05 +0100

    Update serveur2-FastARL.py

--- serveur2-FastARL.py
+++ serveur2-FastARL.py
@@ -5,32 +5,33 @@ import signal
 import time
 import math
 # Create a UDP socket
+
+
+UDP_IP_ADDRESS = "0.0.0.0" #Localhost
+UDP_PORT_NO = int(sys.argv[1])
+mtu=1500-6
+i=1
+j=0
+socket_syn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+socket_syn.bind((UDP_IP_ADDRESS, UDP_PORT_NO))
+
+def get_numseq(i): #pour avoir le format: 00000i
+    if len(str(i))==1:
+        numseq= "00000" + str(i)
+    if len(str(i))==2:
+        numseq= "0000" + str(i)
+    if len(str(i))==3:
+        numseq= "000" + str(i)
+    if len(str(i))==4:
+        numseq= "00" + str(i)
+    if len(str(i))==5:
+        numseq= "0" + str(i)
+    if len(str(i))==6:
+        numseq= str(i)
+    return numseq
+
 while 1:
-    UDP_IP_ADDRESS = "0.0.0.0" #Localhost
-    UDP_PORT_NO = int(sys.argv[1])
-    socket_syn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    mtu=1500-6
-    i=1
-    j=0
-
-    def get_numseq(i): #pour avoir le format: 00000i
-        if len(str(i))==1:
-            numseq= "00000" + str(i)
-        if len(str(i))==2:
-            numseq= "0000" + str(i)
-        if len(str(i))==3:
-            numseq= "000" + str(i)
-        if len(str(i))==4:
-            numseq= "00" + str(i)
-        if len(str(i))==5:
-            numseq= "0" + str(i)
-        if len(str(i))==6:
-            numseq= str(i)
-        return numseq
-
-
-
-    socket_syn.bind((UDP_IP_ADDRESS, UDP_PORT_NO))
     socket_data = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     (message, addrclt) = socket_syn.recvfrom(3)
     message= message.decode()
@@ -59,7 +60,7 @@ while 1:
 
     #------------------------------------ ENVOI DU TABLEAU AU CLIENT -------------------------
     current_segment=1
-    sliding_window= 50 # paramètre à changer pour les test
+    sliding_window= 80 # paramètre à changer pour les test
     list_ack_received=[]
     while current_segment < len(tab_segments): #tant qu'on a pas atteint une valeur superieure au denrier segment
         for segment in tab_segments[current_segment-1:current_segment-1+sliding_window]:#on envoie tous les paquets de la sliding_window
@@ -72,7 +73,10 @@ while 1:
                 socket_data.settimeout(2*(rtt2-rtt1))
             except socket.timeout:
                 continue
-        current_segment=max(list_ack_received)+1
+        if len(list_ack_received)>0:
+            current_segment=max(list_ack_received)+1
+        else:
+            current_segment+=1
     #------------------------------------ FIN TRAITEMENT DES ACK-------------------------------
     socket_data.sendto(b"FIN", addrclt)
     socket_data.close()
